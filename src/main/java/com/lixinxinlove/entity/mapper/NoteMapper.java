cc10e9c989a34d7f20162646f8f8f552f2d2a8cd
lixinxin <895330766@qq.com>
2018-11-01 15:51:08 +0800

    mybatis注解的使用

--- src/main/java/com/lixinxinlove/entity/mapper/NoteMapper.java
+++ src/main/java/com/lixinxinlove/entity/mapper/NoteMapper.java
@@ -1,12 +1,45 @@
 package com.lixinxinlove.entity.mapper;
 
-import org.apache.ibatis.annotations.Insert;
+import com.lixinxinlove.entity.Note;
+import org.apache.ibatis.annotations.*;
 
+import java.util.List;
 import java.util.Map;
 
 public interface NoteMapper {
 
     @Insert("insert into note(user_id,note_text) values(#{user_id,jdbcType=VARCHAR},#{note_text,jdbcType=VARCHAR})")
     int insertByMap(Map<String, Object> map);
+
+
+    @Insert("insert into note(user_id,note_text) values(#{userId,jdbcType=VARCHAR},#{noteText,jdbcType=VARCHAR})")
+    int insertByObject(Note note);
+
+    @Select("select * from note where note_id=#{noteId}")
+    @Results({@Result(column = "note_id", property = "noteId"),
+            @Result(column = "user_id", property = "userId"),
+            @Result(column = "note_text", property = "noteText"),
+            @Result(column = "create_time", property = "createTime"),
+            @Result(column = "update_time", property = "updateTime")})
+    Note findByNoteId(Integer noteId);
+
+
+    @Select("select * from note where user_id=#{userId}")
+    @Results({@Result(column = "note_id", property = "noteId"),
+            @Result(column = "user_id", property = "userId"),
+            @Result(column = "note_text", property = "noteText"),
+            @Result(column = "create_time", property = "createTime"),
+            @Result(column = "update_time", property = "updateTime")})
+    List<Note> findByUserId(String userId);
+
+    @Update("update note set note_text= #{noteText} where note_id=#{noteId}")
+    int updateByNoteId(@Param("noteText") String noteText, @Param("noteId") Integer noteId);
+
+
+    @Update("update note set note_text= #{noteText} where note_id=#{noteId}")
+    int updateByObject(Note note);
+
+    @Delete("delete from note where note_id=#{noteId}")
+    int deleteByNoteId(Integer noteId);
 
 }
--- src/test/java/com/lixinxinlove/mapper/NoteMapperTest.java
+++ src/test/java/com/lixinxinlove/mapper/NoteMapperTest.java
@@ -1,6 +1,7 @@
 package com.lixinxinlove.mapper;
 
 
+import com.lixinxinlove.entity.Note;
 import com.lixinxinlove.entity.mapper.NoteMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RunWith(SpringRunner.class)
@@ -31,5 +33,47 @@ public class NoteMapperTest {
         Assert.assertEquals(1, i);
 
     }
+
+
+    @Test
+    public void insertByObject() throws Exception {
+        Note note = new Note();
+        note.setUserId("0");
+        note.setNoteText("mybatis测试222");
+        int i = mapper.insertByObject(note);
+        Assert.assertEquals(1, i);
+
+    }
+
+
+    @Test
+    public void findByUserId() throws Exception {
+        List<Note> notes = mapper.findByUserId("0");
+        Assert.assertEquals(5, notes.size());
+    }
+
+
+    @Test
+    public void updateByID() {
+        Note note = mapper.findByNoteId(1);
+        int i = mapper.updateByNoteId("updateByID", note.getNoteId());
+        Assert.assertEquals(1, i);
+    }
+
+
+    @Test
+    public void updateByObject() {
+        Note note = mapper.findByNoteId(1);
+        note.setNoteText("updateByObject");
+        int i = mapper.updateByObject(note);
+        Assert.assertEquals(1, i);
+    }
+
+    @Test
+    public void deleteByNoteId() {
+        int i = mapper.deleteByNoteId(5);
+        Assert.assertEquals(1, i);
+    }
+
 
 }
